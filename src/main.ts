391bff9bb5881900c78e0555a3d42c20d0206759
sorachatqq <pleumrinrin@gmail.com>
2026-04-20 18:21:04 +0700

    update: full options enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
 
   const config = new DocumentBuilder()
-    .setTitle('OMS External API')
-    .setDescription('API สำหรับรับ-ส่งข้อมูลปริมาณน้ำมันกับระบบ ASLC')
-    .setVersion('1.0')
+    .setTitle("OMS External API")
+    .setDescription("API สำหรับรับ-ส่งข้อมูลปริมาณน้ำมันกับระบบ ASLC")
+    .setVersion("1.0")
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup("docs", app, document);
 
   const port = process.env.PORT ?? 3201;
   await app.listen(port);
