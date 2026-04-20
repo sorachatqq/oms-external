861c10ac48e83ff7bc51afd1c2ee1c7cb2fab846
sorachatqq <pleumrinrin@gmail.com>
2026-04-20 18:08:27 +0700

    update: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
 
   const config = new DocumentBuilder()
     .setTitle('OMS External API')
